9ee647488eb8fe98f199f24fdb9ee6b4adb7d734
VernihoraIvan <vernihorai@gmail.com>
2024-07-08 14:09:45 +0200

    added routing, created basic pages files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xxs: { max: "460px" },
+        // => @media (max-width: 460px) { ... }
+        xs: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+        sm: { max: "767px" },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+        md: { min: "768px" },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+        lg: { min: "1024px", max: "1279px" },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+        xl: { min: "1200px" },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+      },
+      width: {
+        fit: "fit-content",
+      },
+      spacing: {},
+      colors: {},
+      animation: {},
+    },
   },
   plugins: [],
 };
