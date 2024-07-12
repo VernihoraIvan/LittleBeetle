f0af5dd83d469ed003c5ee4d49492e37cca7cee4
VernihoraIvan <vernihorai@gmail.com>
2024-07-12 13:55:16 +0200

    added main calligraphy title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
+import titleImg from "@/assets/images/book_title.png";
+
 const Home = () => {
   return (
     <div className="flex justify-center pt-headerPad object-fit bg-cover bg-custom-50-150 bg-no-repeat w-screen h-screen bg-home-bg">
       <div className="xxl:w-contW ">
-        <h2 className="font-secondaryRegular text-primWhite text-2xl">
+        <img
+          src={titleImg}
+          alt="a book title"
+          className="w-titleW mb-8 mt-mainTitleM"
+        />
+        <h2 className="font-secondaryRegular text-primWhiteFaint text-linkS">
           This project is a reflection of teamwork and kindness.
         </h2>
       </div>
