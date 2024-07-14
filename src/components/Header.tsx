311799ee0f5a0f24126e5c87c4c8f6e056287f88
VernihoraIvan <vernihorai@gmail.com>
2024-07-14 17:24:42 +0200

    added active effect on navigation elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,29 +2,50 @@
 import { NavLink } from "react-router-dom";
 import Chevron from "@/assets/icons/chevron-down.svg?react";
 import CartLogo from "@/assets/icons/shopping-cart.svg?react";
+import ActiveCartLogo from "@/assets/icons/shopping-cart-active.svg?react";
 
 const Header = () => {
   return (
-    <header className="font-primaryBold z-10 flex flex-center justify-center absolute w-full pt-navPad">
+    <header className="font-primaryBold z-10 flex flex-center justify-center absolute w-full pt-navPad ">
       <nav className="w-contW flex flex-row  text-primWhite text-xl">
         <ul className="mr-navMar flex flex-row w-full justify-between">
           <li>
-            <NavLink to={"/about"}>About</NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) => (isActive ? "text-hovYellow" : "")}
+            >
+              About
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/donation"}>Shop / Donation</NavLink>
+            <NavLink
+              to={"/donation"}
+              className={({ isActive }) => (isActive ? "text-hovYellow" : "")}
+            >
+              Shop / Donation
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/creators"}>Creators</NavLink>
+            <NavLink
+              to={"/creators"}
+              className={({ isActive }) => (isActive ? "text-hovYellow" : "")}
+            >
+              Creators
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/contact"}>Contact</NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) => (isActive ? "text-hovYellow" : "")}
+            >
+              Contact
+            </NavLink>
           </li>
         </ul>
         <ul className="flex flex-row">
           <li className="mr-10 fill-primWhite w-6 h-6">
             <NavLink to={"/cart"}>
-              <CartLogo />
+              {({ isActive }) => (isActive ? <ActiveCartLogo /> : <CartLogo />)}
             </NavLink>
           </li>
           <li className="mr-2">EN</li>
