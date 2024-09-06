447e57e97384fdc69dde88aec48bf73b6a694f35
VernihoraIvan <vernihorai@gmail.com>
2024-09-06 23:02:20 +0200

    added responsive markup for 1440px Creation page display

--- src/components/Elements/CreatorsEl.tsx
+++ src/components/Elements/CreatorsEl.tsx
@@ -6,21 +6,21 @@ const CreatorsEl = ({ name, location, description }: CreatorsElProps) => {
     <div className="py-CreatorsElP  border-b border-primWhiteFaint">
       <div className="flex flex justify-between gap-3">
         <div className="flex flex-col gap-gapS">
-          <h3 className="font-secondaryBold text-buttonS text-primWhite">
+          <h3 className="font-secondaryBold text-buttonS text-primWhite xl:text-linkS">
             {name}
           </h3>
-          <p className="text-secondaryRegular text-2xl text-primWhiteFaint">
+          <p className="text-secondaryRegular text-2xl text-primWhiteFaint xl:text-copyS">
             {location}
           </p>
         </div>
         <div className="h-fit-content cursor-pointer flex justify-center items-center gap-2 border border-primWhiteFaintM py-3  w-60 ">
-          <p className="text-linkS font-primaryRegular text-primWhite">
+          <p className="text-linkS font-primaryRegular text-primWhite xl:text-[16px]">
             Website
           </p>
           <Link />
         </div>
       </div>
-      <p className="py-2 text-primWhite text-linkS font-secondaryRegular">
+      <p className="py-2 text-primWhite text-linkS font-secondaryRegular xl:text-[16px]">
         {description}
       </p>
     </div>
--- src/components/TitleNDescription.tsx
+++ src/components/TitleNDescription.tsx
@@ -4,10 +4,10 @@ const TitleNDescription = ({ title, description }: titleProps) => {
   return (
     <section className="flex justify-center pt-purchImgH pb-bookPB ">
       <div className="flex gap-2 justify-center align-center flex-col xxl:w-contWXXL xl:w-contWXL lg:w-contWLG md:w-contWMD sm:w-contWSM xxs:w-contWXSS">
-        <h2 className="font-primaryBold text-primWhite  text-titleS">
+        <h2 className="font-primaryBold text-primWhite  text-titleS xl:text-buttonS">
           {title}
         </h2>
-        <p className="leading-relaxed text-primWhiteFaint w-[1110px] text-2xl font-secondaryRegular">
+        <p className="leading-relaxed text-primWhiteFaint w-[1110px] text-2xl font-secondaryRegular xl:text-copyS">
           {description}
         </p>
       </div>
