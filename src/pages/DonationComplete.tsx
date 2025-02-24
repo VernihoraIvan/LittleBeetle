4abc1f98c7b47c62cf3317d3998f2a64aab4b140
Ivan Vernihora <vernihorai@gmail.com>
2025-02-24 15:25:48 +0100

    update complete section

--- src/pages/DonationComplete.tsx
+++ src/pages/DonationComplete.tsx
@@ -10,12 +10,12 @@ const DonationComplete = () => {
   return (
     <>
       <Title title="" />
-      <div className=" flex justify-center items-center  h-[calc(100vh-40px)]">
-        <div className="flex flex-col gap-[18px] items-center w-[925px]">
-          <h2 className="font-secondaryBold text-buttonS ">
+      <div className=" flex justify-center items-center  h-[calc(100vh-40px)] px-10">
+        <div className="flex flex-col gap-[18px] items-center max-w-[600px]">
+          <h2 className="font-secondaryBold text-buttonS text-center responsive-heading">
             Thank you! Your donation is complete.
           </h2>
-          <p className="font-secondaryRegular text-linkS text-center">
+          <p className="font-secondaryRegular text-linkS text-center big-responsive-text">
             We sent an email with the access to the digital products included to
             the address you provided. We hope the book and lullaby we've created
             bring you some joy and a touch of magic.
